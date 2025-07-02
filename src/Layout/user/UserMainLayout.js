d3d1841da07efe7ae59f2cae9c93f1ee8bb2481a
MUHAMMED-ASHIQUE-N <muhammedashique433@gmail.com>
2025-07-02 22:40:04 +0530

    making it PWA 4

--- src/Layout/user/UserMainLayout.js
+++ src/Layout/user/UserMainLayout.js
@@ -1,8 +1,8 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { Outlet } from "react-router-dom";
-import Header from "./HeaderBar";
+import Header from "./Header";
 import AppNavbar from "./AppNavbar";
 const UserMainLayout = () => {
-    return (_jsxs("div", { children: [_jsx(Header, { title: null }), _jsx(Outlet, {}), _jsx(AppNavbar, {})] }));
+    return (_jsxs("div", { children: [_jsx(Header, {}), _jsx(Outlet, {}), _jsx(AppNavbar, {})] }));
 };
 export default UserMainLayout;
